abac71e34c54cd97e4e67405e3fcb05228cc79a9
TechnicFreakz <luki.baldner@gmail.com>
2024-08-11 16:07:02 +0200

    Make website work basically the way I want

---
+++ script.js
@@ -0,0 +1,119 @@
+let logic = "or";
+
+const CATEGORIES = [{
+    'tag': 'book',
+    'desc': 'My opinion about a book. May include spoilers!'
+}, {
+    'tag': 'leisure',
+    'desc': 'Just some fun topics I deal with in my free time :)'
+}, {
+    'tag': 'fantasy',
+    'desc': 'Media set in fantasy worlds.'
+}];
+
+function renderPosts() {
+    let activeFilters = Array.from(document.getElementsByClassName('toggle-on')).map(button => button.innerHTML.substring(1));
+    const posts = document.getElementsByClassName('post');
+    for (let post in posts) {    
+        filterTags = Array.from(post.classList).filter(tag => tag != 'post');
+        if (logic == "or") {
+            posts[i].style.display = filterTags.some(e => activeFilters.includes(e)) ? "block" : "none";
+        } else {
+            posts[i].style.display = filterTags.every(e => activeFilters.includes(e)) ? "block" : "none";
+        }
+    }
+}
+
+function renderFilteringButtons() {
+    CATEGORIES.forEach((category)=>{
+        const btn = document.createElement("button");
+        btn.innerHTML = "#" + category['tag'];
+        btn.type = "button";
+        btn.classList = "tooltip toggle button btn-link is-rounded toggle-on toggle-" + category['tag'];
+        btn.title = category["desc"];
+        btn.onclick = function() {
+            btn.classList.toggle('toggle-on');
+            renderPosts();
+        }
+        ;
+
+        const filteringSpan = document.getElementsByClassName("filtering")[0];
+        filteringSpan.appendChild(btn);
+    }
+    )
+}
+
+function toggleAllFilters() {
+    // if at least one off -> all on, else all off
+    let filters = document.getElementsByClassName("toggle");
+    let activeFilters = document.getElementsByClassName("toggle-on");
+
+    if (activeFilters.length == filters.length) {
+        for (let filter of filters) {
+            filter.classList.remove("toggle-on");
+        }
+    } else {
+        for (let filter of filters) {
+            filter.classList.add("toggle-on");
+        }
+    }
+
+    renderPosts()
+}
+
+function toggleTagVisibility() {
+    let currState = document.getElementsByClassName("hide-tags")[0].classList.toggle('hidden');
+    document.getElementsByClassName("show-tags")[0].classList.toggle('hidden');
+
+    CATEGORIES.forEach((className)=>{
+        const elements = document.getElementsByClassName("filtering");
+
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].style.display = currState ? "grid" : "none";
+        }
+    }
+    )
+
+}
+
+function toggleLogic() {
+    let currState = document.getElementsByClassName("and-logic")[0].classList.toggle('hidden');
+    document.getElementsByClassName("or-logic")[0].classList.toggle('hidden');
+
+    if (logic == "or") {
+        logic = "and";
+    } else {
+        logic = "or";
+    }
+
+    renderPosts()
+}
+
+function addTooltips() {
+    let posts = document.getElementsByClassName("post");
+    for (let post of posts) {
+        filterTags = Array.from(post.classList).filter(tag => tag != 'post');
+        var newP = document.createElement('p');
+        newP.classList.add('tags');
+        for (let filter of filterTags) {
+            var newSpan = document.createElement('span');
+            newSpan.classList.add('tag');
+            newSpan.innerHTML = '#' + filter;
+            category = CATEGORIES.find(category => category.tag == filter);
+            if (category == undefined) {
+                console.log(filter + " not in list!")
+            } else {
+                newSpan.setAttribute('title', CATEGORIES.find(category => category.tag == filter)['desc']);
+            }
+            newP.appendChild(newSpan);
+        }
+        post.appendChild(newP);
+    }
+}
+
+function init() {
+    addTooltips();
+    renderFilteringButtons();
+}
+
+window.onload = init
